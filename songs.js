d9dbd68f9b54ef57749a36eb72b409044f393d31
Kakeru-portal <imaginary.628@gmail.com>
2026-08-24 21:16:17 +0900

    データ追加

--- songs.js
+++ songs.js
@@ -1,1 +1,194 @@
-
+// =========================
+// 楽曲データ
+// =========================
+const songData = [
+   {
+ title: "この街で生きている",
+ count: "1",
+ type: "cover",
+ date: "2018-10-04",
+ unit: "",
+ artist: "amazarashi",
+ singer: "夢追翔",
+ video: "ssh61KTPksk",
+ start: 3422,
+ end: 3789
+},
+   {
+ title: "僕が死のうと思ったのは",
+ count: "1",
+ type: "cover",
+ date: "2018-10-04",
+ unit: "",
+ artist: "amazarashi",
+ singer: "夢追翔",
+ video: "ssh61KTPksk",
+ start: 2982,
+ end: 3278
+}
+   ,
+{
+title: "ナモナキヒト",
+count: "1",
+type: "cover",
+date: "2018-10-04",
+unit: "",
+artist: "amazarashi",
+singer: "夢追翔",
+video: "ssh61KTPksk",
+start: 2535,
+end: 2798
+},
+{
+title: "パーフェクトライフ",
+count: "1",
+type: "cover",
+date: "2018-10-04",
+unit: "",
+artist: "amazarashi",
+singer: "夢追翔",
+video: "ssh61KTPksk",
+start: 2080,
+end: 2318
+},
+{
+title: "ジュブナイル",
+count: "1",
+type: "cover",
+date: "2018-10-04",
+unit: "",
+artist: "amazarashi",
+singer: "夢追翔",
+video: "ssh61KTPksk",
+start: 1713,
+end: 1929
+},
+{
+title: "スターライト",
+count: "1",
+type: "cover",
+date: "2018-10-04",
+unit: "",
+artist: "amazarashi",
+singer: "夢追翔",
+video: "ssh61KTPksk",
+start: 1278,
+end: 1520
+},
+{
+title: "名前",
+count: "1",
+type: "cover",
+date: "2018-10-04",
+unit: "",
+artist: "amazarashi",
+singer: "夢追翔",
+video: "ssh61KTPksk",
+start: 850,
+end: 1135
+}
+   ,
+{
+title: "風に流離い",
+count: "1",
+type: "cover",
+date: "2018-10-04",
+unit: "",
+artist: "amazarashi",
+singer: "夢追翔",
+video: "ssh61KTPksk",
+start: 473,
+end: 668
+},
+{
+title: "デビルマンのうた",
+count: "1",
+type: "cover",
+date: "2018-10-01",
+unit: "",
+artist: "十田敬三",
+singer: "夢追翔",
+video: "pZ7MEw0bqEw",
+start: 2,
+end: 41
+},
+{
+title: "Lemon(2018.10.1)",
+count: "1",
+type: "cover",
+date: "2018-10-01",
+unit: "",
+artist: "米津玄師",
+singer: "夢追翔",
+video: "58hvRtSPcGg",
+start: 3449,
+end: 3702
+},
+{
+title: "ray",
+count: "1",
+type: "cover",
+date: "2018-10-01",
+unit: "",
+artist: "BUMP OF CHICKEN",
+singer: "夢追翔",
+video: "58hvRtSPcGg",
+start: 3037,
+end: 3272
+},
+{
+title: "ベンチとコーヒー",
+count: "1",
+type: "cover",
+date: "2018-10-01",
+unit: "",
+artist: "BUMP OF CHICKEN",
+singer: "夢追翔",
+video: "58hvRtSPcGg",
+start: 2611,
+end: 2920
+}
+   ,
+{
+title: "死にたくないから生きている",
+count: "1",
+type: "original",
+date: "2018-09-25",
+unit: "",
+artist: "夢追翔",
+singer: "夢追翔",
+video: "XVVXrJQQuNs"
+},
+{
+title: "Lemon(2018.9.29)",
+count: "1",
+type: "cover",
+date: "2018-09-29",
+unit: "",
+artist: "米津玄師",
+singer: "夢追翔",
+video: "RUJEeuX_Ac0"
+},
+{
+title: "Replace to be",
+count: "5",
+type: "original",
+date: "2026-05-08",
+unit: "VACHSS",
+artist: "VACHSS",
+singer: "VACHSS (夢追翔、加賀美ハヤト、叶、葛葉、剣持刀也、不破湊)",
+video: "7qGbqQTn6og"
+},
+{
+title: "vivi",
+count: "1",
+type: "cover",
+date: "2018-10-01",
+unit: "",
+artist: "米津玄師",
+singer: "夢追翔",
+video: "58hvRtSPcGg",
+start: 2327,
+end: 2520
+}
+];
