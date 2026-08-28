fb02f040a7088c4981095b8aea27827d93d44e97
Kakeru-portal <imaginary.628@gmail.com>
2026-08-28 09:14:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -859,10 +859,7 @@ function createSongCard(song) {
 const songList = document.getElementById('songList');
 if (songList) {
    songData.forEach(song => {
-       const songElement = createSongCard(song);
-       songList.appendChild(songElement);
-       // 保存されているお気に入り状態を反映
-       updateCardFavoriteButton(songElement);
+       songList.appendChild(createSongCard(song));
    });
 }
 filterSongs();
